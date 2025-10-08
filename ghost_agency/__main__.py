d298bf4c3f14c8714339816a634cf6675533fb11
camerensmith <94063173+camerensmith@users.noreply.github.com>
2025-10-08 15:51:30 +0000

    redo

--- ghost_agency/__main__.py
+++
@@ -1,9 +0,0 @@
-from .core.game import Game
-from .core.config import GameConfig
-
-def main():
-    game = Game(GameConfig())
-    game.run()
-
-if __name__ == '__main__':
-    main() 
--- ghost_agency/core/__init__.py
+++
@@ -1,6 +0,0 @@
-from .game_state import GameState, Status
-from .config import GameConfig
-
-__all__ = ['GameState', 'Status', 'GameConfig']
-
-# Core package initialization 
